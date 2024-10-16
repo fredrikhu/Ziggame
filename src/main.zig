3b3240e5f56b063d0a9d4c43b9a2bc6cbb9f90c8
Fredrik Hult <fredrik.hult@outlook.com>
2024-10-16 21:38:42 +0200

    Switch fizzbuzz

--- src/main.zig
+++ src/main.zig
@@ -4,14 +4,14 @@ pub fn main() !void {
     const std_out = std.io.getStdOut().writer();
     var count: u8 = 1;
     while (count <= 100) : (count += 1) {
-        if (count % 5 == 0 and count % 3 == 0) {
-            try std_out.print("fizzbuzz\n", .{});
-        } else if (count % 5 == 0) {
-            try std_out.print("fizz\n", .{});
-        } else if (count % 3 == 0) {
-            try std_out.print("buzz\n", .{});
-        } else {
-            try std_out.print("{}\n", .{count});
+        const div_3: u2 = @intFromBool(count % 3 == 0);
+        const div_5 = @intFromBool(count % 5 == 0);
+
+        switch (div_3 << 1 | div_5) {
+            0b11 => try std_out.print("fizzbuzz\n", .{}),
+            0b10 => try std_out.print("fizz\n", .{}),
+            0b01 => try std_out.print("buzz\n", .{}),
+            else => try std_out.print("{}\n", .{count}),
         }
     }
 }
